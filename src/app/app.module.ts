987f9b4240e63807c75005bd63c8f7b1bfef5362
Володимир Путько <vputko@tvd.local>
2018-11-05 17:49:20 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavHeaderComponent } from './header/nav-header/nav-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    NavHeaderComponent
   ],
   imports: [
     BrowserModule
