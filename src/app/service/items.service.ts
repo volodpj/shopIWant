968247804585d42a4ff9cffee0a9418dbbb1c0c9
volod.pj <volod.af@gmail.com>
2018-11-11 20:53:39 +0200

    item sort

--- src/app/service/items.service.ts
+++ src/app/service/items.service.ts
@@ -7,11 +7,11 @@ import { Item } from '../model/item.model';
 })
 export class ItemsService {
   private items: Item[] = [
-    new Item("Назва товару", "http://obod.com.ua/image/data/images/return-box.jpg", 1, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
-    new Item("Назва товару", "http://obod.com.ua/image/data/images/return-box.jpg", 2, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
-    new Item("Назва товару", "http://obod.com.ua/image/data/images/return-box.jpg", 3, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
-    new Item("Назва товару", "http://obod.com.ua/image/data/images/return-box.jpg", 4, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
-    new Item("Назва товару", "http://obod.com.ua/image/data/images/return-box.jpg", 5, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
+    new Item("Товар1", "http://obod.com.ua/image/data/images/return-box.jpg", 1, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
+    new Item("Товар2", "http://obod.com.ua/image/data/images/return-box.jpg", 2, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
+    new Item("Товар3", "http://obod.com.ua/image/data/images/return-box.jpg", 3, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
+    new Item("Товар4", "http://obod.com.ua/image/data/images/return-box.jpg", 4, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
+    new Item("Товар5", "http://obod.com.ua/image/data/images/return-box.jpg", 5, true, 190, "Опис товару", "SONY", "Розділ1", "Підрозділ2", true, true, true, true),
   ];
 
   constructor() { }
