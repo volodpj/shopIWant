a9dc15027ad7fbd140a5471b37699d633f9054de
volod.pj <volod.af@gmail.com>
2018-11-26 21:32:51 +0200

    routes1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes } from '@angular/router'
 import { MainSecondComponent } from './main/main-second/main-second.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
@@ -13,5 +14,7 @@ import { MainSecondComponent } from './main/main-second/main-second.component';
 export class AppRoutingModule { }
 
 export const ROUTES: Routes = [
-  { path: "*", component: MainSecondComponent },
+  { path: "baseMain", component: MainComponent },
+  { path: "secondMain", component: MainSecondComponent },
+  { path: "", redirectTo: "/baseMain", pathMatch: "full"},
 ]
